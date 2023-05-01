3797ba1f857fbf3d1be32c6061ba3cf5bb4dd419
sup1214 <sup1214@naver.com>
2023-05-01 23:57:40 +0900

    코드리뷰

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mobile-view">
+        <h1>Mobile View</h1>
+        <p>Hi All, Explore https://drimmake.com/ for learn more.</p>
+      </div>
     </div>
   );
 }
